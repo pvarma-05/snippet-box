931c56b7874d426ec11230b40af88e01fcbe07a2
Pradeep Varma <pradeeppenumatsa555@gmail.com>
2025-09-01 09:30:33 +0530

    Completed Chapter 2.4

--- main.go
+++ main.go
@@ -1,26 +1,34 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello From SnippetBox"))
 }
 
-func snippetView(w http.ResponseWriter,r *http.Request){
-	w.Write([]byte("Displaying specific Snippet"))
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	id,err := strconv.Atoi(r.PathValue("id"))
+	if err!= nil || id<1{
+		http.NotFound(w,r)
+		return
+	}
+	msg := fmt.Sprintf("Displaying a specific snippet with ID: %d",id)
+	w.Write([]byte(msg))
 }
 
-func snippetCreate(w http.ResponseWriter,r *http.Request){
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Displaying a form for creating a new snippet"))
 }
 
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/{$}", home)
-	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/view/{id}", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	log.Print("Server Listening on Port 4000")
