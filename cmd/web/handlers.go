813d37d940efe9244e948da4e5b3e7c6cb148fc8
Pradeep Varma <pradeeppenumatsa555@gmail.com>
2025-09-01 17:42:31 +0530

    Completed Chapter 2.7

---
+++ cmd/web/handlers.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Server", "Go")
+	w.Write([]byte("Hello from SnippetBox"))
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.PathValue("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprint(w, "Displaying the Snippet with id :",id)
+}
+
+func snippetCreate(w http.ResponseWriter,r *http.Request){
+	w.Write([]byte("Display a form form for creating a snippet"))
+}
+
+func snippetCreatePost(w http.ResponseWriter,r *http.Request){
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte("Saving a new Snippet..."))
+}
