da3a606ff8a05d8fc64f85a10d71087173623ea1
Pradeep Varma <pradeeppenumatsa555@gmail.com>
2025-09-01 18:36:50 +0530

    Completed Chapter 2.8

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,13 +2,30 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Server", "Go")
-	w.Write([]byte("Hello from SnippetBox"))
+	files := []string{
+		"./ui/html/pages/base.tmpl",
+		"./ui/html/pages/nav.tmpl",
+		"./ui/html/pages/home.tmpl",
+	}
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
+	err = ts.ExecuteTemplate(w,"base", nil)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
+
 }
 
 func snippetView(w http.ResponseWriter, r *http.Request) {
@@ -17,14 +34,14 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 		http.NotFound(w, r)
 		return
 	}
-	fmt.Fprint(w, "Displaying the Snippet with id :",id)
+	fmt.Fprint(w, "Displaying the Snippet with id :", id)
 }
 
-func snippetCreate(w http.ResponseWriter,r *http.Request){
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Display a form form for creating a snippet"))
 }
 
-func snippetCreatePost(w http.ResponseWriter,r *http.Request){
+func snippetCreatePost(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusCreated)
 	w.Write([]byte("Saving a new Snippet..."))
 }
